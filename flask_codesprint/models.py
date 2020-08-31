b50c4cd4e6fd4744e854857e41b6202da466b11a
Ankit Chawla <ankitc226@gmail.com>
2020-08-31 23:47:35 +0530

    Added registration page

--- flask_codesprint/models.py
+++ flask_codesprint/models.py
@@ -1,6 +1,11 @@
-from flask_codesprint import db
+from flask_codesprint import db,login_manage
+from flask_login import UserMixin
 
-class User(db.Model):
+@login_manage.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model,UserMixin):
 
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(100),nullable=False,unique=True)
--- flask_codesprint/routes.py
+++ flask_codesprint/routes.py
@@ -1,19 +1,36 @@
-from flask_codesprint import app
+from flask_codesprint import app,db
 from flask import render_template,redirect,url_for,flash,request
 from flask_codesprint.models import User
 from flask_codesprint import bcrypt
-from flask_login import login_user
+from flask_login import login_user,current_user
 
-@app.route('/')
+"""@app.route('/')
 def home():
-  return 'Welcome! <a href="/login">login here</a>'
+  return 'Welcome! <a href="/login">login here</a>"""
 
-@app.route('/login',methods = ['GET','POST'])
+@app.route('/',methods = ['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return render_template('main.html')
+
     if request.method == 'POST':
         user = User.query.filter_by(username=request.form['username']).first()
         if user and bcrypt.check_password_hash(user.password,request.form['password']):
-            return "Hello bro.You have been logged in"
+            return render_template('main.html')
         else:
             flash('Login unsuccessfull. Please check username and password.','danger')
     return render_template('login.html')
+
+@app.route('/register',methods = ['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        return render_template('main.html')
+
+    if request.method == 'POST':
+        hashed_pass = bcrypt.generate_password_hash(request.form['password']).decode('utf-8')
+        user = User(username=request.form['username'],password = hashed_pass)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
